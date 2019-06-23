d1d38b4f7e94f6819f7b4376d25f64ab1b3c084e
Jason Wolf <jessy.yameogo@gmail.com>
2019-06-23 01:07:11 -0400

    completed: defining data models

---
+++ lib/models/option.dart
@@ -0,0 +1,12 @@
+class Option {
+  String value;
+  String detail;
+  bool correct;
+
+  Option({this.correct, this.value, this.detail});
+  Option.fromMap(Map data) {
+    value = data['value'];
+    detail = data['detail'] ?? '';
+    correct = data['correct'];
+  }
+}
